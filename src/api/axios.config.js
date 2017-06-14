683d64f4e32229f08c78411a510f8571d416b7ca
Utopia <greatauk11@gmail.com>
2017-06-14 11:59:17 +0800

    add axios config to make use axios easy

---
+++ src/api/axios.config.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import qs from 'qs'
+// import * as _ from '../util/tool'
+
+//最长响应时间
+axios.defaults.timeout = 10000;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+axios.defaults.baseURL = '/api';
+// axios.defaults.baseURL = '真实后台 url';
+
+//POST传参序列化(添加请求拦截器)
+axios.interceptors.request.use((config) => {
+  //在发送请求之前做某件事
+  if(config.method  === 'post'){
+    config.data = qs.stringify(config.data);
+  }
+  return config;
+},(error) =>{
+    _.toast("错误的传参", 'fail');
+  return Promise.reject(error);
+});
+
+//返回状态判断(添加响应拦截器)
+axios.interceptors.response.use((res) =>{
+  //对响应数据做些事
+    if(!res.data.success){
+        // _.toast(res.data);
+        return Promise.reject(res);
+    }
+    return res;
+}, (error) => {
+    _.toast("网络异常", 'fail');
+    return Promise.reject(error);
+});
+
+export default axios;
